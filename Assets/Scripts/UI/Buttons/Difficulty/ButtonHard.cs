a7a5dac880149dee11b7d46dad7a15c439d162f4
KHS <kij4300@naver.com>
2023-10-16 15:56:18 +0900

    난이도
    쉬움, 어려움 난이도 버튼 만들었다.

---
+++ Assets/Scripts/UI/Buttons/Difficulty/ButtonHard.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHard : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("어려움 난이도 선택했습니다.");
+    }
+}
