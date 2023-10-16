40111d1095a7e141d5b6b46d7bb1ac8a86f3d216
KHS <kij4300@naver.com>
2023-10-16 16:14:17 +0900

    설정 버튼
    나머지 씬에도 설정 버튼을 넣었다.

---
+++ Assets/Scripts/UI/Buttons/ButtonSetting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSetting : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Debug.Log("설정 버튼을 눌렀습니다.");
+    }
+}
