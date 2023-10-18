b228c9a4aee5beb63542e1448ec7f0774c87b00c
kksoo0131 <a67636521@gmail.com>
2023-10-18 18:45:06 +0900

    아이템 조합 기능 UI까지는 오나성

--- Assets/Scripts/Inventory/UIInventory.cs
+++ Assets/Scripts/Inventory/UIInventory.cs
@@ -5,7 +5,11 @@ public class UIInventory : MonoBehaviour
 {
 
     public UIItemSlot[] inventorySlot;
-    Item[] itemInSlot;
+
+    public Dictionary<string, int> itemCount = new Dictionary<string, int>();// 어떤아이템이 몇개있는지
+
+    public UICombination UICombination;
+    Item[] itemInSlot;// 아이템이 슬롯의 어느위치에 있는지
     List<int> EmptySlot;
 
     private void Awake()
@@ -27,6 +31,23 @@ public class UIInventory : MonoBehaviour
         }
     }
 
+    public void AddToDictinary(string name)
+    {
+        if (itemCount.ContainsKey(name))
+        {
+            itemCount[name]++;
+        }
+        else
+        {
+            itemCount.Add(name, 1);
+        }
+    }
+
+    public void RemoveFromDictionary(Item item)
+    {
+        itemCount[item.itemSO.itemName]--;
+    }
+
     public bool AddItemToInventory(Item item) 
     {
         if (EmptySlot.Count <= 0) return false;
@@ -36,8 +57,11 @@ public class UIInventory : MonoBehaviour
         int index = EmptySlot[0];
         EmptySlot.RemoveAt(0);
 
+        AddToDictinary(item.itemSO.itemName);
         itemInSlot[index] = item;
         inventorySlot[index].AddItemUI(item.itemSO);
+
+        UICombination.UpdateUICombination();
 
         return true;
     }
@@ -46,12 +70,16 @@ public class UIInventory : MonoBehaviour
     {
         if (itemInSlot[index] == null) return;
 
+        Item item = itemInSlot[index];
+        itemInSlot[index] = null;
+
         EmptySlot.Add(index);
         inventorySlot[index].RemoveItemUI();
 
-        itemInSlot[index].gameObject.SetActive(true);
+        item.gameObject.SetActive(true);
         // 아이템의 위치를 플레이어의 위치로 변경해줘야됨 (드랍)
-        itemInSlot[index] = null;
+        RemoveFromDictionary(item);
+        UICombination.UpdateUICombination();
     }
 }
 
