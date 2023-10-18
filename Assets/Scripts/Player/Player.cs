36aa2e06146d36220f1fbef6333d29b00f76f5dd
cplusplusdev <cplusplusdev@naver.com>
2023-10-18 22:08:28 +0900

    플레이어 이동 애니메이션 적용

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -24,7 +24,6 @@ public class Player : MonoBehaviour
     private void Awake()
     {
 
-        m_rigidbody = GetComponent<Rigidbody>();
         Debug.Assert(m_rigidbody != null);
 
     }
@@ -32,18 +31,28 @@ public class Player : MonoBehaviour
 
     private void Move(Vector2 direction)
     {
-        m_direction = new Vector3(direction.x, 0.0f, direction.y);
+        m_direction = direction;
+        m_animator.SetFloat("MoveSpeed", m_direction.magnitude);
     }
 
     private void FixedUpdate()
     {
-        m_rigidbody.velocity = m_direction * m_speed * Time.fixedDeltaTime;
+
+        m_rigidbody.velocity = transform.forward * m_direction.y * m_moveSpeed * Time.fixedDeltaTime;
+
+        transform.localRotation = Quaternion.Euler(0.0f, m_direction.x * m_rotationSpeed * Time.fixedDeltaTime, 0.0f) * transform.localRotation;
+
     }
 
-    private Rigidbody   m_rigidbody;
+    [SerializeField] private Rigidbody   m_rigidbody;
 
     //Movement
-    [SerializeField] private float  m_speed;
-    private Vector3                 m_direction;
+    [SerializeField] private float  m_moveSpeed;
+    [SerializeField] private float  m_rotationSpeed;
+    private Vector2                 m_direction;
+
+
+    //Animation
+    [SerializeField] Animator m_animator;
 
 }
